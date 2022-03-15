9b6a4af4c5be33a8164c64a3ccc64e9a4de7e540
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-03-15 14:28:56 -0500

    :recycle: add projects to data skew

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,47 @@
+import b00stImage from '../public/static/images/b00st_screen.webp'
+import phundImage from '../public/static/images/phund_xyz.webp'
+import slightlyImage from '../public/static/images/slightly_sharpe.webp'
+import tincreDevImage from '../public/static/images/tincre-dev.webp'
+import lottaImage from '../public/static/images/lotta-moberg-phd-cfa.webp'
+import stevensServicesImage from '../public/static/images/stevens-services.jpg'
+
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Tincre Developer Platform',
+    description: `Tincre solves the web's last mile. APIs and integrations for developers while powering Tincre's one-click no-code solutions.`,
+    imgSrc: tincreDevImage,
+    href: 'https://tincre.dev',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'b00st.com',
+    description: `A better way to market music for artists, labels, management companies and their partners.`,
+    imgSrc: b00stImage,
+    href: 'https://b00st.com',
+  },
+  {
+    title: 'phund.xyz',
+    description: 'Fund your raise the hard way and keep your equity for yourself.',
+    imgSrc: phundImage,
+    href: 'https://phund.xyz',
+  },
+  {
+    title: 'The Slightly Sharpe Blog',
+    description:
+      'A digital outline of a hand-held mobile device and a bright source of alternative, new information about the web, business, finance, and software.',
+    imgSrc: slightlyImage,
+    href: 'https://slightlysharpe.com',
+  },
+  {
+    title: 'lottamoberg.com',
+    description: `Lotta Moberg, PhD, CFA's site for blog, opinion, research, and media posts.`,
+    imgSrc: lottaImage,
+    href: 'https://lottamoberg.com',
+  },
+  {
+    title: 'Stevens Services LLC',
+    description: `Whether it's lawn and landscape, snow and ice management, or construction and remodeling, Stevens Services brings an experienced team to solve customer challenges. Let them help solve yours.`,
+    imgSrc: stevensServicesImage,
+    href: 'https://stevens-services.com',
   },
 ]
 
