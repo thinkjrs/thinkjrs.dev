b349d041816ebf55904550f9bf9e1fdc9227efb9
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-06-18 20:42:49 -0500

    :sparkles: add tincre.com and investor.tincre.com

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,6 +2,7 @@ import b00stImage from '../public/static/images/b00st_screen.webp'
 import phundImage from '../public/static/images/phund_xyz.webp'
 import slightlyImage from '../public/static/images/slightly_sharpe.webp'
 import tincreDevImage from '../public/static/images/tincre-dev.webp'
+import tincreBrandImage from '../public/static/images/tincre-brand.webp'
 import lottaImage from '../public/static/images/lotta-moberg-phd-cfa.webp'
 import stevensServicesImage from '../public/static/images/stevens-services.jpg'
 
@@ -11,6 +12,12 @@ const projectsData = [
     description: `Tincre solves the web's last mile. APIs and integrations for developers while powering Tincre's one-click no-code solutions.`,
     imgSrc: tincreDevImage,
     href: 'https://tincre.dev',
+  },
+  {
+    title: 'tincre.com',
+    description: `Tincre creates value on the web by solving the web's last mile. Tincre builds technology and brands to bind consumption and intent with revenue`,
+    imgSrc: tincreBrandImage,
+    href: 'https://tincre.com',
   },
   {
     title: 'b00st.com',
@@ -23,6 +30,12 @@ const projectsData = [
     description: 'Fund your raise the hard way and keep your equity for yourself.',
     imgSrc: phundImage,
     href: 'https://phund.xyz',
+  },
+  {
+    title: 'Tincre Investor Site',
+    description: `A home for up-to-date data on Tincre's business, built with open source Phund.xyz!`,
+    imgSrc: tincreBrandImage,
+    href: 'https://investor.tincre.com',
   },
   {
     title: 'The Slightly Sharpe Blog',
